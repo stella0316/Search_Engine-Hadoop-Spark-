a9f92d14890f30b013cef3ed95a1cd4e558250ba
Will-Zhiwei <zy1129@nyu.edu>
2018-04-21 21:48:38 -0400

    sample column index added

--- Zhiwei/column_index/column_index.py
+++ Zhiwei/column_index/column_index.py
@@ -5,8 +5,11 @@ from operator import add
 from collections import Counter
 import string
 import re
+#from pyspark.sql import Row
+#from pyspark.sql import SparkSession
 
 sc = SparkContext()
+#spark = SparkSession.builder.appName("column index").config("spark.some.config.option", "some-value").getOrCreate()
 
 data = sc.textFile(sys.argv[1], 1)
 header = data.first()
@@ -34,23 +37,36 @@ data = data.map(lambda x: (x[0], parse(x[1])))
 
 def create_pairs(pair):
     '''
-        Produce key value pairs: (word, frequency, (doc_id, column))
+        Produce key value pairs:
+            (word, [[doc_id, column], ..., [doc_id, column]])
     '''
     doc_id = pair[0]
     table = string.maketrans('!@#$%^&*()_+-=,./;<>?:', '                      ')
     output = []
     for column in pair[1]:
-        column = column.translate(table)
-        column = column.lower()
-        words = column.split(' ')
-        words = map(lambda x: x.strip(), words)
-        words = filter(None, words)
-        #counter = Counter(words)
-        #for word, count in counter.items():
-        #   output.append((word, (doc_id, column, count)))
-        for word in words:
-            output.append((word, (doc_id, column)))
+        column_norm = column.translate(table)
+        column_norm = column_norm.lower()
+        words = column_norm.split(' ')
+        #words = set(words)
+        words = filter(lambda x: x is not '', words)
+        counter = Counter(words)
+        for word, count in counter.items():
+            output.append((word, [[doc_id, column, count]]))
+        #for word in words:
+            #output.append((word, [[doc_id, column]]))
     return output
 
-inverted_index = data.flatMap(create_pairs).reduceByKey(lambda x,y: x+y).map(lambda x: (x[0], len(x[1])/2, x[1]))
+def print_result(x):
+    result = x[0] + '\t' + str(x[1]) + '\t' + '('
+    for location in x[2]:
+        result += '(' + location[0] + ',' + '\'' + location[1] + '\'' + ')' + ','
+    result = result[0:-1]
+    result += ')'
+    return result
+
+
+inverted_index = data.flatMap(create_pairs).reduceByKey(lambda x,y: x+y).map(lambda x: (x[0], len(x[1]), x[1])).sortBy(lambda x: -x[1]).map(lambda x: (x[0], x[1], sorted(x[2], key=lambda y: -y[2]))).map(print_result)#map(lambda x: Row(keyword=x[0], frequency=x[1], location_list=str(x[2])))
 inverted_index.saveAsTextFile("column_index.out")
+#column_index = spark.createDataFrame(inverted_index)
+#column_index.show()
+#column_index.coalesce(1).write.save("column_index.csv",format="csv",header="true")
