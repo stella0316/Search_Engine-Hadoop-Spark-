1ddeefa87666c1f839b3eeaaf13914dee9d9f8d9
ksenialearn <ksenia.sayenko@gmail.com>
2018-04-24 14:31:57 -0400

    latest json

--- Ksenia/json/test_file_input.py
+++ Ksenia/json/test_file_input.py
@@ -8,6 +8,9 @@ import pandas as pd
 import subprocess
 import json
 
+'''
+Process data into a desired csv form. Drop metadata
+'''
 sc = SparkContext()
 hadoop = sc._jvm.org.apache.hadoop
 fs = hadoop.fs.FileSystem
@@ -17,24 +20,16 @@ for f in fs.get(conf).listStatus(path):
 	file_path = str(f.getPath())
 	cat = subprocess.Popen(["hadoop","fs","-cat",file_path],stdout=subprocess.PIPE)
 	out = cat.communicate()[0]
-	json_string = out.decode('utf-8')
+	if len(out)==0:
+		print('empty')
+		continue
+	#json_string = out.decode('utf-8')
+	print('Processing: ', file_path)
 	data_chunk = json.loads(out.decode('utf-8'))
-	#print(data_chunk)
 	column_infos = data_chunk['meta']['view']['columns']
 	column_names = [info['name'] for info in column_infos]
-	#print(column_names)
 	data_rows = data_chunk['data']
-	# print(data_chunk['meta'])
 	out = pd.DataFrame(data_rows, columns = column_names)
+	file_name = data_chunk['meta']['view']['name'] 
+	out.to_csv('nyc_processed_data/'+file_name+'.csv')
 	print('Done with', file_path)
-	out.to_csv('test.csv')
-
-  #rdd = sc.wholeTextFiles(temp.json)
-  #data = spark.read.json(rdd)
-  #print(data.first())
-#data = sc.read.json('hdfs:/user/bigdata/nyc_open_data/22zm-qrtq.json')
-#path = sys.argv[1]
-#rdd = sc.textFile(path)
-
-# print(data.first())
-
