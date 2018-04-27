eeef1ed4a98b95da4fec27e9b2ee252a1c17be4c
ksenialearn <ksenia.sayenko@gmail.com>
2018-04-27 14:06:12 -0400

    latest json updates

--- Ksenia/json/test_file_input.py
+++ Ksenia/json/test_file_input.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from pyspark import SparkContext
+from pyspark import SparkContext as sc
 from pyspark.sql import SparkSession
 import pandas as pd
 from pyspark.sql.types import *
@@ -11,25 +11,34 @@ import json
 '''
 Process data into a desired csv form. Drop metadata
 '''
-sc = SparkContext()
+spark = SparkSession \
+	.builder \
+	.appName("Python Spark SQL basic example") \
+	.config("spark.some.config.option", "some-value") \
+	.getOrCreate()
+
+#sc.hadoopConfiguration.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
 hadoop = sc._jvm.org.apache.hadoop
 fs = hadoop.fs.FileSystem
 conf = hadoop.conf.Configuration()
 path = hadoop.fs.Path('hdfs:/user/bigdata/nyc_open_data/')
+count=0
 for f in fs.get(conf).listStatus(path):
+	#if count == 100:
+		#break
 	file_path = str(f.getPath())
-	cat = subprocess.Popen(["hadoop","fs","-cat",file_path],stdout=subprocess.PIPE)
-	out = cat.communicate()[0]
-	if len(out)==0:
-		print('empty')
-		continue
+	#cat = subprocess.Popen(["hadoop","fs","-cat",file_path],stdout=subprocess.PIPE)
+	#out = cat.communicate()[0]
+	#if len(out)==0:
+		#print('empty')
+		#continue
 	#json_string = out.decode('utf-8')
 	print('Processing: ', file_path)
-	data_chunk = json.loads(out.decode('utf-8'))
-	column_infos = data_chunk['meta']['view']['columns']
-	column_names = [info['name'] for info in column_infos]
-	data_rows = data_chunk['data']
-	out = pd.DataFrame(data_rows, columns = column_names)
-	file_name = data_chunk['meta']['view']['name'] 
-	out.to_csv('nyc_processed_data/'+file_name+'.csv')
-	print('Done with', file_path)
+	df = spark.read.json(file_path, multiLine=True)
+	rddd = df.select('data').rdd
+	mapped = rddd.flatMap(lambda x: x[0])
+	out = mapped.map(lambda x: x[8:-1])
+	name = df.select('meta.view.name').collect()[0][0]
+	out.saveAsTextFile('processed_data/' + name)
+	count+=1
+	print('Processed: ', str(count))
