5a96b6f27b9fd65513a87b071ebe6f1ed85c390b
ksenialearn <ksenia.sayenko@gmail.com>
2018-05-03 21:47:54 -0400

    clean directory

--- main1/bash_code.sh
+++
@@ -1,9 +0,0 @@
-module load python/gnu/3.4.4
-module load spark/2.2.0
-
-NAME="$1"
-
-spark-submit --conf \
-spark.pyspark.python=/share/apps/python/3.4.4/bin/python \
-test_prompt.py "$NAME" 
-
--- main1/call_spark.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-to_read = input("enter file name: ")
-print(to_read)
-process = subprocess.check_output(["bash","cal_p.sh",to_read],stdout = subprocess.PIPE)
-print(process.stdout)
-#process.stdout.readline()
-
-
