b64ff29d375c8056b6fbf57cdaff38fd361201dd
Will-Zhiwei <zy1129@nyu.edu>
2018-05-05 14:09:34 -0400

    minor

--- main/call_program.sh
+++ main/call_program.sh
@@ -8,4 +8,4 @@ FILTER="$3"
 
 spark-submit --conf \
 spark.pyspark.python=/share/apps/python/3.4.4/bin/python \
-maini.py /user/ss8955/project/title_index.txt /user/ss8955/project/column_index.txt /user/ss8955/project/content_index.txt /user/ss8955/project/Master_Index_New.csv /user/ss8955/project/tag_index.csv /user/ss8955/project/Table_Desc_2.csv "$SEARCH_TYPES" "$WORDS" "$FILTER" /user/ss8955/project/Table_with_Columns.csv
+main.py /user/ss8955/project/title_index.txt /user/ss8955/project/column_index.txt /user/ss8955/project/content_index.txt /user/ss8955/project/Master_Index_New.csv /user/ss8955/project/tag_index.csv /user/ss8955/project/Table_Desc_2.csv "$SEARCH_TYPES" "$WORDS" "$FILTER" /user/ss8955/project/Table_with_Columns.csv
