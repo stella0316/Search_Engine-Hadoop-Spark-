3fa76b3198415154c82b1d8a8aafc46c2df59c34
ksenialearn <ksenia.sayenko@gmail.com>
2018-05-08 15:21:25 -0400

    process json files

--- Ksenia/json/dump_json1.sh
+++
@@ -1,5 +0,0 @@
-module load python/gnu/3.4.4
-module load spark/2.2.0
-
-spark-submit --conf \
-spark.pyspark.python=/share/apps/python/3.4.4/bin/python process_json.py
--- Ksenia/json/process_json.sh
+++ Ksenia/json/process_json.sh
@@ -2,4 +2,4 @@ module load python/gnu/3.4.4
 module load spark/2.2.0
 
 spark-submit --conf \
-spark.pyspark.python=/share/apps/python/3.4.4/bin/python test_file_input.py
+spark.pyspark.python=/share/apps/python/3.4.4/bin/python process_json_csv.py
