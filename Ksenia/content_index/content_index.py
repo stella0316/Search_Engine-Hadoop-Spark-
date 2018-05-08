ea4140537a5228d7f1c77574a1a212b517d72b05
ksenialearn <ksenia.sayenko@gmail.com>
2018-05-08 15:13:14 -0400

    index processing

--- Ksenia/content_index/content_index.py
+++ Ksenia/content_index/content_index.py
@@ -7,23 +7,45 @@ Extract all words from each document and match each document to the keys
 import sys
 from csv import reader
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 from operator import add
 from collections import Counter
+import pandas as pd
+from itertools import chain
+import nltk
+
+porter = nltk.stem.PorterStemmer()
+stopWordList = set(nltk.corpus.stopwords.words("english"))
 
 sc = SparkContext()
 
+spark = SparkSession \
+  .builder \
+  .appName("Session") \
+  .config("spark.some.config.option", "some-value") \
+  .getOrCreate()
+
 #read metadata table and map to (file_name, id)
-id_table = sc.textFile('Table_Describe.csv').mapPartitions(lambda line: reader(line, delimiter=',',quotechar='"')).filter(lambda line: line[0]!='Doc_ID').map(lambda x: (x[1],x[0]))
+id_table = sc.textFile('Master_Index_New.csv').mapPartitions(lambda line: reader(line, delimiter=',',quotechar='"')).filter(lambda line: line[0]!='Doc_ID').map(lambda x: (x[2],x[0]))
+id_vals = id_table.collect() #convert to a list
 
-#read files from folder 
-data = sc.wholeTextFiles('data/*')
-path = 'hdfs://dumbo/user/ks4841/data/'
-len_path = len(path)
-len_csv = len('.csv')
-data = data.map(lambda x: (x[0][len_path:-len_csv],x[1]))#extract file name from path
+temped = False
 
-#Match file names with ID-> (ID,Text)
-joined = data.leftOuterJoin(id_table).map(lambda y: (y[1][1],y[1][0]))
+for pair in id_vals:
+	file_name = pair[0]
+	file_id = pair[1]
+	data = spark.read.format('csv').options(header=True, inferSchema=True).load('hdfs://dumbo/user/ks4841/nyc_processed_data/' + file_name + '.csv')
+	#if data.count()>1000000: #if more than a million rows
+	#	continue
+	data = data.rdd.map(tuple).map(lambda line: (file_id, line)) #add file id to each line of file
+	#print(data.first())
+	if temped:
+		joined = joined.union(data)
+	else:
+		joined = data.cache()
+		temped = True
+	
+#print(joined.take(2))
 
 
 def create_pairs(pair):
@@ -31,40 +53,45 @@ def create_pairs(pair):
   Produce key value pairs: (word,frequency)(doc_id, word_frequency))
   '''
   doc_id = pair[0]
-  name = pair[1]
-  table = str.maketrans(dict.fromkeys('\n()\"!-,',' '))
-  name = name.translate(table) #remove unnecessary elements
-  name = name.lower() #convert to lower case
-  name.replace
-  words = name.split(' ')                                                                                                                 
-  words = map(lambda x: x.strip(),words)#remove trailing whitespaces                                                                      
-  words = filter(None, words)#remove Nans                                                                                                 
-  counter = Counter(words)                                                                                                                                                                                                             
-  output = []                                                                                                                             
-  for word, count in counter.items():                                                                                                     
-    output.append((word,count))                                                                                                                                                                                
-  output = map(lambda x: (x[0],((doc_id, x[1]),)),output)                                                                                 
+  text = pair[1]
+  df = pd.Series(text)
+  df = df.apply(str)
+  #df = df[df.apply(lambda x: isinstance(x,str))]#remove all non-strings
+  table = str.maketrans(dict.fromkeys('\n()\*,"!-',' '))
+  df = df.str.translate(table) #remove unnecessary elements
+  df = df.str.lower() #convert to lower case
+  df = df.str.split()
+  word_list = df.tolist()
+  #word_list = [porter.stem(x) for x in word_list if x not in stopWordList]
+  words = list(chain.from_iterable(word_list))
+  words = [porter.stem(x) for x in words if x not in stopWordList]
+	#words = map(lambda x: x.strip(),words)#remove trailing whitespaces                                                                      
+  #words = filter(None, words)#remove Nans                                                                                                 
+  counter = Counter(words)     
+
+  output = [] 
+  for word, count in counter.items():
+    output.append((word,count))                                                                                                         
+  output = map(lambda x: ((x[0], doc_id), x[1]),output)                                                                                 
   return output  
 
 def format_output(line):
-	'''
-	Format string for output
-	'''
-	doc_list = line[2]
-	docs = []
-	for doc in doc_list:
-		docs.append(doc[0])
-	docs = ','.join(docs)
-	output = line[0] + '\t' + str(line[1]) + '\t' + '(' + docs + ')'
-	return output
-
+  '''
+  #Format string for output
+  '''
+  doc_list = line[2]
+  docs = []
+  for doc in doc_list:
+    docs.append(doc[0])
+  docs = ','.join(docs)
+  output = line[0] + '\t' + str(line[1]) + '\t' + '(' + docs + ')'
+  return output
 
 #Preprocess text data: remove title, spaces, unnecessary symbols.
 inverted_index = joined.flatMap(create_pairs).reduceByKey(lambda a,b: a+b)
+inverted_index = inverted_index.map(lambda line: (line[0][0],((line[0][1],line[1]),))).reduceByKey(lambda a,b: a+b)
+#print(inverted_index.take(2))
 inverted_index = inverted_index.map(lambda line: (line[0], len(line[1]), line[1])).sortBy(lambda x: x[1],ascending=False)\
-	.map(lambda x: (x[0], x[1], sorted(x[2], key = lambda y: -y[1]))).map(format_output) #organize and sort
+.map(lambda x: (x[0], x[1], sorted(x[2], reverse=True))).map(format_output) #organize and sort
 print(inverted_index.take(2)) #print to check the output looks correct
-
-inverted_index.saveAsTextFile("content_index.txt")   
-
-
+inverted_index.saveAsTextFile("content_index.txt")
--- Ksenia/content_index/spark-submit.sh
+++ Ksenia/content_index/spark-submit.sh
@@ -1,19 +1,5 @@
-
 module load python/gnu/3.4.4
 module load spark/2.2.0
 
-FILENAME="content_index.txt"
-
-/usr/bin/hadoop fs -rm -r "$FILENAME"
-
 spark-submit --conf \
-spark.pyspark.python=/share/apps/python/3.4.4/bin/python \
-content_index.py
-
-rm "$FILENAME"
-
-/usr/bin/hadoop fs -getmerge "$FILENAME" "$FILENAME"
-
-
-
-
+spark.pyspark.python=/share/apps/python/3.4.4/bin/python content_index.py
