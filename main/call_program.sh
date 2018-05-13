5391be58d859d2340dd5659925b634dcd996ef44
Ksenia Saenko <ksenia.sayenko@gmail.com>
2018-05-12 21:13:24 -0400

    Update call_program.sh

--- main/call_program.sh
+++ main/call_program.sh
@@ -8,4 +8,6 @@ FILTER="$3"
 
 spark-submit --conf \
 spark.pyspark.python=/share/apps/python/3.4.4/bin/python \
-main.py search/title_index.txt search/column_index.txt search/content_index.txt search/Master_Index_New.csv search/tag_index.csv search/Table_Desc_2.csv "$SEARCH_TYPES" "$WORDS" "$FILTER" search/Table_with_Columns.csv
+main.py final_data/title_index.txt final_data/column_index.txt final_data/content_index.txt \
+final_data/Master_Index_New.csv final_data/tag_index.csv \
+final_data/Table_Desc_2.csv "$SEARCH_TYPES" "$WORDS" "$FILTER" final_data/Table_with_Columns.csv
